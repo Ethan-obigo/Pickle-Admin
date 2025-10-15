b3b3d404a2652bbf5daa5a0c15b88e145ea4415e
정창우 <wjdckddn99@naver.com>
2025-10-15 11:23:07 +0900

    feat: 빈 데이터 안내문구 추가

--- src/EpisodeList.tsx
+++ src/EpisodeList.tsx
@@ -18,35 +18,41 @@ const EpisodeList = ({ data }: { data: excelProps[] }) => {
       ref={parentRef}
       className="w-full h-[75%] overflow-auto border-t border-gray-300"
     >
-      <div
-        style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-          position: "relative",
-        }}
-      >
-        {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-          const epi = data[virtualRow.index];
-          return (
-            <div
-              key={virtualRow.key}
-              ref={rowVirtualizer.measureElement}
-              className="absolute left-0 right-0 flex items-center border-b border-gray-200 cursor-pointer hover:bg-gray-100 transition"
-              style={{
-                transform: `translateY(${virtualRow.start}px)`,
-                height: `${virtualRow.size}px`,
-              }}
-            >
-              <p className="w-[10%] px-2">{epi.episodeId}</p>
-              <p className="w-[20%] line-clamp-2 px-2">{epi.episodeName}</p>
-              <p className="w-[15%] px-2">{epi.episodeType}</p>
-              <p className="w-[15%] line-clamp-2 px-2">{epi.channelName}</p>
-              <p className="w-[10%] px-2">{epi.likeCnt}</p>
-              <p className="w-[10%] px-2">{epi.playTime}</p>
-              <p className="w-[15%] px-2">{formatDateString(epi.createdAt)}</p>
-            </div>
-          );
-        })}
-      </div>
+      {data.length > 0 ? (
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            position: "relative",
+          }}
+        >
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const epi = data[virtualRow.index];
+            return (
+              <div
+                key={virtualRow.key}
+                ref={rowVirtualizer.measureElement}
+                className="absolute left-0 right-0 flex items-center border-b border-gray-200 cursor-pointer hover:bg-gray-100 transition"
+                style={{
+                  transform: `translateY(${virtualRow.start}px)`,
+                  height: `${virtualRow.size}px`,
+                }}
+              >
+                <p className="w-[10%] px-2">{epi.episodeId}</p>
+                <p className="w-[20%] line-clamp-2 px-2">{epi.episodeName}</p>
+                <p className="w-[15%] px-2">{epi.episodeType}</p>
+                <p className="w-[15%] line-clamp-2 px-2">{epi.channelName}</p>
+                <p className="w-[10%] px-2">{epi.likeCnt}</p>
+                <p className="w-[10%] px-2">{epi.playTime}</p>
+                <p className="w-[15%] px-2">
+                  {formatDateString(epi.createdAt)}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="w-full h-full flex justify-center items-center">새로 등록된 에피소드가 존재하지 않습니다.</div>
+      )}
     </div>
   );
 };
