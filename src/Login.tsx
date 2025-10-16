f29910fbec4cae81341be0cac0284e6e4aa9f177
Ethan <Ethan.Jung@obigo.com>
2025-10-16 17:14:10 +0900

    fix: 로그인 창 닫힘 버그 수정

--- src/Login.tsx
+++ src/Login.tsx
@@ -80,7 +80,10 @@ export default function LoginPopup({
       className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
       onClick={onClose}
     >
-      <div className="relative bg-white p-8 rounded-xl w-80 flex flex-col gap-4">
+      <div
+        className="relative bg-white p-8 rounded-xl w-80 flex flex-col gap-4"
+        onClick={(e) => e.stopPropagation()}
+      >
         <button
           onClick={onClose}
           className="absolute top-4 cursor-pointer right-6 text-md text-gray-500 mt-2"
